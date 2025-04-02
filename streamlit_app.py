7397f6f80f8c898f0b38da7be38670e8ffb0b8b9
vishux777 <vishwas.23@lpu.in>
2025-04-02 11:31:09 +0530

    ending......

--- streamlit_app.py
+++ streamlit_app.py
@@ -246,7 +246,7 @@ def main():
         
         # Credits
         st.markdown("---")
-        st.caption("© 2025 SmartSpend")
+        st.caption("© 2025 SmartSpend #Created by VK")
 
     # Main Content
     st.title("SmartSpend Assistant")
